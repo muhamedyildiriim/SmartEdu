ebd9ed553e28f1e8d9065930825f1e44a9f8b4af
Muhammed YILDIRIM <mby342458@gmail.com>
2024-06-25 13:59:13 +0300

    routes folder are created

---
+++ routes/pageRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const pageController = require('../controllers/pageControllers');
+
+const router = express.Router();
+
+router.route('/').get(pageController.getIndexPage);
+router.route('/about').get(pageController.getAboutPage);
+router.route('/courses').get(pageController.getCoursesPage);
+
+module.exports = router;
