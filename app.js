fe5b275a77d0eb4871c04f4d7ceda7b2216db934
Muhammed YILDIRIM <mby342458@gmail.com>
2024-06-26 14:41:30 +0300

    Files in partials folder are set for global (every href get /), course-single file turned into course and set for .ejs, courses page are created and otomatically setted for new courses with ejs write method, new courses are created with postman before create course page created

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoutes');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
 
-// TEMPLATE ENGINE
+//CONNECT DB
+// mongoose.connect('mongodb://localhost/smartedu-db')
+// .then(console.log('Connected to DB succesfully'));
+mongoose.connect('mongodb://localhost/smartedu-db', {})
+.then(() => {
+  console.log('DB Connected Successfully');
+});
+
+//TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// ROUTES
+//ROUTES
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
--- controllers/pageControllers.js
+++ controllers/pageControllers.js
@@ -9,21 +9,3 @@ exports.getAboutPage = (req, res) => {
     page_name: 'about',
   });
 };
-
-exports.getCoursesPage = (req, res) => {
-  res.status(200).render('courses', {
-    page_name: 'courses',
-  });
-};
-
-exports.getDashboardPage = (req, res) => {
-  res.status(200).render('dashboard', {
-    page_name: 'dashboard',
-  });
-};
-
-exports.getContactPage = (req, res) => {
-  res.status(200).render('contact', {
-    page_name: 'contact',
-  });
-};
