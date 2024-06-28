8d2d6a8e3207e3ead5f2e837e7e0e4dff51381ed
Muhammed YILDIRIM <mby342458@gmail.com>
2024-06-28 12:02:08 +0300

    Mongo connect package imported and sessions are saved

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,6 +4,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.route('/signup').post(authController.createUser); //  https://localhost:3000/users/signup
-router.route('/login').post(authController.loginUser); //  https://localhost:3000/users/signup
+router.route('/login').post(authController.loginUser); //  https://localhost:3000/users/login
+router.route('/logout').get(authController.logoutUser); //  https://localhost:3000/users/logout
 
 module.exports = router;
