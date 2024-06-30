fc1ff63125ee26c4099ae1a4df6e8b462d2cec07
Muhammed YILDIRIM <mby342458@gmail.com>
2024-06-30 12:42:12 +0300

    Enroll mechanism is integrated to project.

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,10 +1,16 @@
 const Course = require('../models/Course');
 const Category = require('../models/Category');
+const User = require('../models/User');
 
 exports.createCourse = async (req, res) => {
 
   try {
-    const course = await Course.create(req.body);
+    const course = await Course.create({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      user: req.session.userID
+    });
 
     res.status(201).redirect('/courses')
 
@@ -30,7 +36,7 @@ exports.getAllCourses = async (req, res) => {
       filter = {category:category._id}
     }
 
-    const courses = await Course.find(filter);
+    const courses = await Course.find(filter).sort('-createdAt');
     const categories = await Category.find({});
 
     res.status(200).render('courses', {
@@ -51,7 +57,7 @@ exports.getAllCourses = async (req, res) => {
 exports.getCourse = async (req, res) => {
 
   try {
-    const course = await Course.findOne({slug: req.params.slug});
+    const course = await Course.findOne({slug: req.params.slug}).populate('user');
 
     res.status(200).render('course', {
       course,
@@ -64,4 +70,21 @@ exports.getCourse = async (req, res) => {
       error,
     });
   }
+};
+
+exports.enrollCourse = async (req, res)=> {
+
+  try {
+    const user = await User.findById(req.session.userID);
+    await user.courses.push({ _id : req.body.course_id });
+    await user.save();
+
+    res.status(200).redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+
 };
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -4,8 +4,9 @@ const roleMiddleware = require('../middlewares/rolesMiddleware');
 
 const router = express.Router();
 
-router.route('/').post(roleMiddleware(["teacher", "admin"]), courseController.createCourse); // https://localhost:3000/courses
-router.route('/').get(courseController.getAllCourses); // http://localhost:3000/courses
+router.route('/').post(roleMiddleware(["teacher", "admin"]), courseController.createCourse);
+router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
+router.route('/enroll').post(courseController.enrollCourse);
 
 module.exports = router;
