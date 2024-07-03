e0f7c7fa2627d47b6c7a8a46956ccb1408d12edf
Muhammed YILDIRIM <mby342458@gmail.com>
2024-07-03 17:46:14 +0300

    Contact page and form created

--- controllers/pageControllers.js
+++ controllers/pageControllers.js
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render('index', {
@@ -21,4 +23,50 @@ exports.getLoginPage = (req, res) => {
   res.status(200).render('login', {
     page_name: 'login',
   });
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+};
+
+exports.sendEmail = async (req, res) => {
+
+  const outputMessage = `
+  <h1>Mail Details</h1>
+  <ul>
+    <li>${req.body.name}</li>
+    <li>${req.body.email}</li>
+  </ul>
+  <h1> Message </h1>
+  <p>${req.body.message}</p>
+  `
+
+  const nodemailer = require("nodemailer");
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "my382884@gmail.com",
+      pass: "tomn qxrs mfyw sopb",
+    },
+  });
+
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Smart Edu Contact Form 👻" <my382884@gmail.com>', // sender address
+    to: "mby342458@gmail.com", // list of receivers
+    subject: "Smart Edu Contact Form New Message ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: outputMessage, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+
+  res.status(200).redirect('contact');
+
 };
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -7,5 +7,8 @@ router.route('/').get(pageController.getIndexPage); // http://localhost:3000/ind
 router.route('/about').get(pageController.getAboutPage); // http://localhost:3000/about
 router.route('/register').get(pageController.getRegisterPage); // http://localhost:3000/register
 router.route('/login').get(pageController.getLoginPage); // http://localhost:3000/login
+router.route('/contact').get(pageController.getContactPage); // http://localhost:3000/contact
+router.route('/contact').post(pageController.sendEmail); // http://localhost:3000/contact
 
+//tomn qxrs mfyw sopb
 module.exports = router;
