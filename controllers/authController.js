3c24cf22ce973e1bfb38bdc6a2935b1a56dd78ec
Muhammed YILDIRIM <mby342458@gmail.com>
2024-07-03 10:19:58 +0300

    Auth login situation fixed.

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,15 +21,23 @@ exports.loginUser = async (req, res) => {
 
   if (spesificUser) {
     bcrypt.compare(password, spesificUser.password, (err, same) => {
+
       if (same) {
         //USER SESSION
         req.session.userID = spesificUser._id;
         res.status(200).redirect('/users/dashboard');
+
       } else {
         res.status(400).json({
           status: 'fail',
         });
       }
+
+    });
+
+  } else {
+    res.status(400).json({
+      status: 'fail',
     });
   }
 };
@@ -43,11 +51,11 @@ exports.logoutUser = (req, res) => {
 exports.getDashboardPage = async (req, res) => {
   const user = await User.findOne({ _id: req.session.userID });
   const categories = await Category.find();
-  const courses = await Course.find({user:req.session.userID})
+  const courses = await Course.find({ user: req.session.userID });
   res.status(200).render('dashboard', {
     page_name: 'dashboard',
     user,
     categories,
-    courses
+    courses,
   });
 };
