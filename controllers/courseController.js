6eb54f5433311f1b0c771c85d437876835ffa210
Muhammed YILDIRIM <mby342458@gmail.com>
2024-07-06 13:20:00 +0300

    Update feature added.

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -132,8 +132,29 @@ exports.deleteCourse = async (req, res) => {
   try {
 
     const course = await Course.findOneAndDelete({slug:req.params.slug});
+    const users = await User.find({courses:{$in:course}}).updateMany({$pull:{'courses':course._id}});
     res.status(200).redirect('/users/dashboard')
     req.flash('error', `${course.name} has been delete!`);
+
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.updateCourse = async (req, res) => {
+  try {    
+
+    const course = await Course.findOne({slug:req.params.slug});
+    course.name = req.body.name;
+    course.description = req.body.description;
+    course.category = req.body.category;
+
+    course.save();
+
+    res.status(200).redirect('/users/dashboard');
 
   } catch (error) {
     res.status(400).json({
