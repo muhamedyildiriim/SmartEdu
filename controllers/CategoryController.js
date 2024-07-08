5d73d3d09767bc89f528dce47777f96942817ee4
Muhammed YILDIRIM <mby342458@gmail.com>
2024-07-08 11:30:44 +0300

    Category add and category delete functions has been added to admin dashboard page.

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,14 +1,26 @@
 const Category = require('../models/Category');
 
 exports.createCategory = async (req, res) => {
-
   try {
     const category = await Category.create(req.body);
+    res.status(201).redirect('/users/dashboard')
 
-    res.status(201).json({
-      status: 'success',
-      category,
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
     });
+  }
+};
+
+
+
+exports.deleteCategory = async (req, res) => {
+  try {
+
+    const category = await Category.findById(req.params._id);
+    await Category.deleteOne(category);
+    res.status(201).redirect('/users/dashboard');
 
   } catch (error) {
     res.status(400).json({
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -4,5 +4,6 @@ const categoryController = require('../controllers/categoryController');
 const router = express.Router();
 
 router.route('/').post(categoryController.createCategory); // https://localhost:3000/categories
+router.route('/:_id').delete(categoryController.deleteCategory); // https://localhost:3000/categories
 
 module.exports = router;
